5c43bdaa0bd8a95df566c556821fec7610dfa8eb
Takayuki <s0303618@gmail.com>
2025-03-22 17:37:17 +0900

    contactページの完成

--- footer.php
+++ footer.php
@@ -1,29 +1,31 @@
 <div class="toTop">
-      <a class="toTop__link" href="<?php echo esc_url(home_url()) ?>"><i class="fa-solid fa-arrow-up"></i></a>
+  <a class="toTop__link" href="<?php echo esc_url(home_url()) ?>"><i class="fa-solid fa-arrow-up"></i></a>
+</div>
+<footer class="footer">
+  <div class="footer__upper">
+    <div class="footer__inner">
+      <div class="footer__contentWrap">
+        <div class="footer__logo">
+          <img src="<?php echo esc_url(get_theme_file_uri('img/logo.png')) ?>" alt="たかゆきポートフォリオのロゴ">
+        </div>
+        <div class="footer__snsWrap">
+          <ul class="footer__snsList">
+            <li class="footer__snsItem">
+              <a class="footer__snsLink" href="https://x.com/TakayukiTech"><i class="fa-brands fa-x-twitter"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <footer class="footer">
-      <div class="footer__upper">
-        <div class="footer__inner">
-          <div class="footer__contentWrap">
-            <div class="footer__logo">
-              <img src="<?php echo esc_url(get_theme_file_uri('img/logo.png')) ?>" alt="たかゆきポートフォリオのロゴ">
-            </div>
-            <div class="footer__snsWrap">
-              <ul class="footer__snsList">
-                <li class="footer__snsItem">
-                  <a class="footer__snsLink" href="https://x.com/TakayukiTech"><i class="fa-brands fa-x-twitter"></i></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+  </div>
 
-      <div class="footer__under">
-        <div class="footer__inner">
-          <p class="footer__copyright">&copy; takayuki portfolio all rights 2025</p>
-        </div>
-      </div>
-    </footer>
+  <div class="footer__under">
+    <div class="footer__inner">
+      <p class="footer__copyright">&copy; takayuki portfolio all rights 2025</p>
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
