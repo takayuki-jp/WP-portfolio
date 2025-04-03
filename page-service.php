d105f25d14cd2b61d355fdc9a2d76f9a390c93c7
Takayuki <s0303618@gmail.com>
2025-04-03 19:46:59 +0900

    サービスページ仕上げ

--- page-service.php
+++ page-service.php
@@ -33,30 +33,49 @@
               <span class="char char-7">制</span>
               <span class="char char-8">作</span>
             </h2>
+
             <div class="svSection__contentWrap">
               <div class="svSection__price">
                 <h3 class="svSection__priceTitle">料金一例</h3>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
+                <div class="svSection__priceContentWrap">
+                  <div class="svSection__priceDefListWrap">
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">【トップページ】<br>デザイン＋コーディング</dt>
+                      <dd class="svSection__priceDataDesc">30,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">【トップページ】<br>デザイン or コーディング</dt>
+                      <dd class="svSection__priceDataDesc">20,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">【下層ページ】<br>デザイン＋コーディング</dt>
+                      <dd class="svSection__priceDataDesc">15,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">【下層ページ】<br>デザイン or コーディング</dt>
+                      <dd class="svSection__priceDataDesc">10,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">WordPressへの実装</dt>
+                      <dd class="svSection__priceDataDesc">+30,000<span>円~</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">WordPressプラグイン実装</dt>
+                      <dd class="svSection__priceDataDesc">+5,000<span>円〜</span></dd>
+                    </dl>
+                  </div>
+                  <ul class="svSection__priceNoteList">
+                    <li class="svSection__priceNoteItem">※商品やサービスの画像提供をお願いします。</li>
+                    <li class="svSection__priceNoteItem">※こちらで用意する画像については基本的に著作権フリー素材を使用します。</li>
+                    <li class="svSection__priceNoteItem">※料金は制作物のサイズによって上下します。詳しくはお問い合わせにてご相談ください。</li>
+                  <ul>
+                </div>
               </div>
               <div class="svSection__commentWrap svSection__commentWrap--website">
                 <p class="svSection__comment">
                   Webサイトの制作を承ります。<br>
-                  デザインからコーディングまではもちろん、デザインのみ、コーディングのみ、トップページのみ、新規ページのみ等、それぞれのご要望に応じて柔軟に対応いたします。
+                  デザインからコーディングまではもちろん、デザインのみ、コーディングのみ、トップページのみ、追加したいページのみ等、それぞれのご要望に応じて柔軟に対応いたします。<br>
+                  また、WordPressでのカスタムテーマデザインの実装も可能です。<br>
                 </p>
               </div>
             </div>
@@ -74,26 +93,33 @@
               <span class="char char-6">制</span>
               <span class="char char-7">作</span>
             </h2>
+
             <div class="svSection__contentWrap svSection__contentWrap--lp">
               <div class="svSection__price svSection__price--lp">
                 <h3 class="svSection__priceTitle">料金一例</h3>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1ページ</dt>
-                  <dd class="svSection__priceData">10,000円</dd>
-                </dl>
+                <div class="svSection__priceContentWrap">
+                  <div class="svSection__priceDefListWrap">
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">デザイン＋コーディング</dt>
+                      <dd class="svSection__priceDataDesc">50,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">コーディングのみ</dt>
+                      <dd class="svSection__priceDataDesc">30,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">デザインのみ</dt>
+                      <dd class="svSection__priceDataDesc">30,000<span>円〜</span></dd>
+                    </dl>
+                  </div>
+                  <ul class="svSection__priceNoteList">
+                    <li class="svSection__priceNoteItem">※商品やサービスの画像提供をお願いします。</li>
+                    <li class="svSection__priceNoteItem">※こちらで用意する画像については基本的に著作権フリー素材を使用します。</li>
+                    <li class="svSection__priceNoteItem">※料金は制作物のサイズによって上下します。詳しくはお問い合わせにてご相談ください。</li>
+                  <ul>
+                </div>
               </div>
+
               <div class="svSection__commentWrap svSection__commentWrap--lp">
                 <p class="svSection__comment">
                   LPページ制作を承ります。<br>
@@ -119,22 +145,35 @@
               <span class="char char-10">制</span>
               <span class="char char-11">作</span>
             </h2>
+
             <div class="svSection__contentWrap">
               <div class="svSection__price">
                 <h3 class="svSection__priceTitle">料金一例</h3>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">1枚</dt>
-                  <dd class="svSection__priceData">5,000円</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">サイズ展開</dt>
-                  <dd class="svSection__priceData">2,000円 / 枚</dd>
-                </dl>
-                <dl class="svSection__priceDefList">
-                  <dt class="svSection__priceDataTitle">【オプション】早仕上げ（２日以内）</dt>
-                  <dd class="svSection__priceData">＋3,000円</dd>
-                </dl>
+                <div class="svSection__priceContentWrap">
+                  <div class="svSection__priceDefListWrap">
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">1枚</dt>
+                      <dd class="svSection__priceDataDesc">5,000<span>円〜</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">サイズ展開<span>※1</span></dt>
+                      <dd class="svSection__priceDataDesc">2,000<span>円~/枚</span></dd>
+                    </dl>
+                    <dl class="svSection__priceDefList">
+                      <dt class="svSection__priceDataTitle">早仕上げ<span>※2</span></dt>
+                      <dd class="svSection__priceDataDesc">+3,000<span>円~</span></dd>
+                    </dl>
+                  </div>
+                  <ul class="svSection__priceNoteList">
+                    <li class="svSection__priceNoteItem">※1 同じデザインでサイズを変える場合の料金です。</li>
+                    <li class="svSection__priceNoteItem">※2 通常よりも早く納品する場合の料金です。</li>
+                    <li class="svSection__priceNoteItem">※商品やサービスの画像提供をお願いします。</li>
+                    <li class="svSection__priceNoteItem">※こちらで用意する画像については基本的に著作権フリー素材を使用します。</li>
+                    <li class="svSection__priceNoteItem">※料金は制作物のサイズによって上下します。詳しくはお問い合わせにてご相談ください。</li>
+                  <ul>
+                </div>
               </div>
+
               <div class="svSection__commentWrap svSection__commentWrap--banner">
                 <p class="svSection__comment">
                   各種画像制作を承ります。<br>
